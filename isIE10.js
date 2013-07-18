5791383dd6080a8e3cedc4c244a473bb2601b6ee
kolodny <kolodny.github@gmail.com>
2013-07-18 15:53:44 -0400

    Update isIE10.js

--- isIE10.js
+++ isIE10.js
@@ -1,10 +1,8 @@
 window.isIE10 = (function(){
 	var matches = navigator.userAgent.match(/MSIE\s?(\d+)(?:\.(\d+))?/i);
 		
-	if (matches) {
-		if (+matches[1] === 10) {
-			return true;
-		}
+	if (matches && +matches[1] === 10) {
+		return true;
 	}
 	return false;
 })();
